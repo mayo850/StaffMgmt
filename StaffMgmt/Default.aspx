a96bd9bcf2bbe17285e74551c458548c9b8aa31c
mayo850 <90479591+mayo850@users.noreply.github.com>
2025-02-02 19:24:58 -0600

    Add project files.

---
+++ StaffMgmt/Default.aspx
@@ -0,0 +1,90 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GrantStaffManagement.Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title class="text-align-center">Staff Management</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="Styles/default.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <div class="heading mb-3 text-center">CPRD Staff</div>
+            <div class="line mb-3"></div>
+
+            <div class="row mb-4">
+                <div class="col-md-6">
+                    <asp:Label ID="GrantLabel" runat="server" Text="Select Grant:"></asp:Label>
+                    <asp:DropDownList ID="GrantDropdown" runat="server" AutoPostBack="true" OnSelectedIndexChanged="GrantDropdown_SelectedIndexChanged" CssClass="form-select"></asp:DropDownList>
+                </div>
+                <div class="col-md-6">
+                    <asp:Label ID="StaffActiveLabel" runat="server" Text="Staff Active:"></asp:Label>
+                    <asp:DropDownList ID="StaffActiveDropdown" runat="server" AutoPostBack="true" Enabled="true" OnSelectedIndexChanged="StaffActiveDropdown_SelectedIndexChanged" CssClass="form-select">
+                        <asp:ListItem Text="--Select--" Value="" />
+                        <asp:ListItem Text="Yes" Value="Yes" />
+                        <asp:ListItem Text="No" Value="No" />
+                    </asp:DropDownList>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="staff-list-title">Staff List</div>
+                    <div class="staff-list">
+                        <asp:ListBox ID="StaffList" runat="server" AutoPostBack="true" OnSelectedIndexChanged="StaffList_SelectedIndexChanged" Width="100%" Height="100%"></asp:ListBox>
+                    </div>
+                </div>
+
+                <div class="col-md-8 info-section">
+                    <div class="mb-3 row">
+                        <label for="NameField" class="col-sm-3 col-form-label">Name:</label>
+                        <div class="col-sm-9">
+                            <asp:TextBox ID="NameField" runat="server" CssClass="form-control" />
+                        </div>
+                    </div>
+
+                    <div class="mb-3 row">
+                        <label for="StartDateField" class="col-sm-3 col-form-label">Start Date:</label>
+                        <div class="col-sm-4">
+                            <div class="input-group">
+                                <asp:TextBox ID="StartDateField" runat="server" CssClass="form-control" />
+                                <span class="input-group-text"><i class="fas fa-calendar-alt"></i></span>
+                            </div>
+                        </div>
+
+                        <label for="EndDateField" class="col-sm-2 col-form-label">End Date:</label>
+                        <div class="col-sm-3">
+                            <div class="input-group">
+                                <asp:TextBox ID="EndDateField" runat="server" CssClass="form-control" />
+                                <span class="input-group-text"><i class="fas fa-calendar-alt"></i></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="mb-3 row">
+                        <label for="EmailField" class="col-sm-3 col-form-label">Email:</label>
+                        <div class="col-sm-9">
+                            <asp:TextBox ID="EmailField" runat="server" CssClass="form-control" />
+                        </div>
+                    </div>
+
+                    <div class="mb-3 row">
+                        <label for="CertificationDateField" class="col-sm-3 col-form-label">Certification Date:</label>
+                        <div class="col-sm-9">
+                            <div class="input-group">
+                                <asp:TextBox ID="CertificationDateField" runat="server" CssClass="form-control" />
+                                <span class="input-group-text"><i class="fas fa-calendar-alt"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ StaffMgmt/Default.aspx.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace GrantStaffManagement
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //ppulate Grant Dropdown on page load
+                GrantDropdown.DataSource = PopulateGrantDropdown();
+                GrantDropdown.DataBind();
+            }
+        }
+
+        protected void GrantDropdown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //enable Staff Active Dropdown when a grant is selected
+            StaffActiveDropdown.Enabled = true;
+        }
+
+        protected void StaffActiveDropdown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //populate Staff List based on selected grant and staff active status
+            string grantName = GrantDropdown.SelectedValue;
+            string staffActiveStatus = StaffActiveDropdown.SelectedValue;
+
+            StaffList.DataSource = PopulateStaffList(grantName, staffActiveStatus);
+            StaffList.DataBind();
+        }
+
+        protected void StaffList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string staffName = StaffList.SelectedValue;
+            string grantName = GrantDropdown.SelectedValue;
+
+            var staffDetails = GetStaffDetails(staffName, grantName);
+
+            NameField.Text = staffDetails["StaffName"];
+            StartDateField.Text = staffDetails["StartDate"];
+            EndDateField.Text = staffDetails["EndDate"];
+            EmailField.Text = staffDetails["Email"];
+            CertificationDateField.Text = staffDetails["CertificationDate"];
+        }
+
+        private Dictionary<string, string> GetStaffDetails(string staffName, string grantName)
+        {
+            Dictionary<string, string> staffDetails = new Dictionary<string, string>();
+
+            using (SqlConnection connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["GrantStaffDB"].ConnectionString))
+            {
+                connection.Open();
+                string query = @"
+                    SELECT s.StaffName, sga.StartDate, sga.EndDate, s.Email, s.CertificationDate 
+                    FROM Staff s
+                    JOIN StaffGrantAssignment sga ON s.StaffID = sga.StaffID
+                    JOIN GrantInfo g ON sga.GrantID = g.GrantID
+                    WHERE s.StaffName = @StaffName AND g.GrantName = @GrantName";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@StaffName", staffName);
+                    command.Parameters.AddWithValue("@GrantName", grantName);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            staffDetails["StaffName"] = reader["StaffName"].ToString();
+                            staffDetails["StartDate"] = reader["StartDate"].ToString();
+                            staffDetails["EndDate"] = reader["EndDate"] == DBNull.Value ? "Active" : reader["EndDate"].ToString();
+                            staffDetails["Email"] = reader["Email"].ToString();
+                            staffDetails["CertificationDate"] = reader["CertificationDate"].ToString();
+                        }
+                    }
+                }
+            }
+
+            return staffDetails;
+        }
+
+        //db helper mmethods
+        private List<string> PopulateGrantDropdown()
+        {
+            List<string> grants = new List<string>();
+
+            using (SqlConnection connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["GrantStaffDB"].ConnectionString))
+            {
+                connection.Open();
+                string query = "SELECT GrantName FROM GrantInfo";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            grants.Add(reader["GrantName"].ToString());
+                        }
+                    }
+                }
+            }
+
+            return grants;
+        }
+
+        private List<string> PopulateStaffList(string grantName, string staffActiveStatus)
+        {
+            List<string> staffList = new List<string>();
+
+            using (SqlConnection connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["GrantStaffDB"].ConnectionString))
+            {
+                connection.Open();
+                string query = @"
+                    SELECT s.StaffName 
+                    FROM Staff s
+                    JOIN StaffGrantAssignment sga ON s.StaffID = sga.StaffID
+                    JOIN GrantInfo g ON sga.GrantID = g.GrantID
+                    WHERE g.GrantName = @GrantName 
+                      AND (@StaffActiveStatus = 'Yes' AND sga.EndDate IS NULL 
+                           OR @StaffActiveStatus = 'No' AND sga.EndDate IS NOT NULL)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@GrantName", grantName);
+                    command.Parameters.AddWithValue("@StaffActiveStatus", staffActiveStatus);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            staffList.Add(reader["StaffName"].ToString());
+                        }
+                    }
+                }
+            }
+
+            return staffList;
+        }
+    }
+}
